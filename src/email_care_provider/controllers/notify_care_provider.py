57c2e53e26fa3c638205e92fec8d613669deaa1a
Michał Kras <michal.kras@thepsc.co.uk>
2023-03-11 17:01:00 +0100

    HANS-204: Remove prof data

---
+++ src/email_care_provider/controllers/notify_care_provider.py
@@ -0,0 +1,6 @@
+from notifications_python_client.notifications import NotificationsAPIClient
+
+
+class NotifyCareProviderController:
+    def __init__(self, notifications_api_client: NotificationsAPIClient):
+        pass
