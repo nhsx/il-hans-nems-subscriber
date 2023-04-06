49f54133b33a5dac523cb1496fe306588c7b1acd
Michał Kras <michal.kras@thepsc.co.uk>
2023-04-06 13:21:38 +0200

    HANS-209: Implement processing batches

--- src/email_care_provider/app.py
+++ src/email_care_provider/app.py
@@ -9,12 +9,13 @@ _LOGGER = Logger()
 
 @_LOGGER.inject_lambda_context(log_event=True)
 def lambda_handler(event: dict, context: LambdaContext):
-    bundle = HANSBundle.parse_raw(event["body"])
-    NotifyCareProviderController().send_email_to_care_provider(
-        patient_nhs_number=bundle.patient.identifier[0].value,
-        patient_given_name=bundle.patient.name[0].given[0],
-        patient_family_name=bundle.patient.name[0].family,
-        patient_birth_date=bundle.patient.birthDate,
-        location_name=bundle.location.name,
-        admitted_at=bundle.encounter.period.start,
-    )
+    for queue_message in event:
+        bundle = HANSBundle.parse_raw(queue_message["body"])
+        NotifyCareProviderController().send_email_to_care_provider(
+            patient_nhs_number=bundle.patient.identifier[0].value,
+            patient_given_name=bundle.patient.name[0].given[0],
+            patient_family_name=bundle.patient.name[0].family,
+            patient_birth_date=bundle.patient.birthDate,
+            location_name=bundle.location.name,
+            admitted_at=bundle.encounter.period.start,
+        )
